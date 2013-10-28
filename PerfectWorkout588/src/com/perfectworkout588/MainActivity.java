9ef6d1ac3fb68d71a26c5859b6a50c29d78a9527
arioch666 <divij.durve@gmail.com>
2013-10-28 01:14:09 -0700

    added the code to detect proximity. It starts the countdown
    Signed-off-by: arioch666 <divij.durve@gmail.com>

--- PerfectWorkout588/src/com/perfectworkout588/MainActivity.java
+++ PerfectWorkout588/src/com/perfectworkout588/MainActivity.java
@@ -1,18 +1,30 @@
 package com.perfectworkout588;
 
+
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener{
 
 	private int time; //in seconds
 	private TextView mintv, sectv;
+	Sensor accelerometer, proximitySensor;
+	SensorManager sensorManager;
+	Boolean started;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +33,7 @@ public class MainActivity extends Activity {
 		
 		Button button;
 		time = 70;
-		
+		started = false;
 		mintv = (TextView)findViewById(R.id.tvMin);
 		mintv.setText(Integer.toString(time/60));
 		
@@ -33,8 +45,134 @@ public class MainActivity extends Activity {
 		
 		button = (Button)findViewById(R.id.btnReset);
 		button.setOnClickListener(resetTimer);
+		initilizeSensors();
+		
 	}
 
+	private void initilizeSensors()
+	{
+		if(initializeAccelerometer())
+		{
+			
+			if(initializeProximitySensor())
+			{
+				Toast.makeText(this, "Initilization Complete", Toast.LENGTH_SHORT).show();		
+				
+			}
+			else
+			{
+				Toast.makeText(this, "initilization failed", Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
+	
+	private boolean initializeProximitySensor()
+	{
+		Sensor defaultProximitySensor = ((SensorManager) 
+				getSystemService(Context.SENSOR_SERVICE)).getDefaultSensor(Sensor.TYPE_PROXIMITY);
+		
+		if (defaultProximitySensor == null)
+		{
+			Toast.makeText(this, "Your Device Does not have a proximity sensor. Buy a better Device", Toast.LENGTH_SHORT).show();
+			return false;
+		} else
+		{
+			proximitySensor = defaultProximitySensor;
+			Toast.makeText(this, "Proximity Sensor set", Toast.LENGTH_SHORT).show();
+			return true;
+		}
+	}
+
+	private Boolean initializeAccelerometer()
+	{
+		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		
+		Sensor defaultAccelerometer = ((SensorManager) 
+				getSystemService(Context.SENSOR_SERVICE)).getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		
+		if (defaultAccelerometer == null)
+		{
+			Toast.makeText(this, "Your Device Does not have an accelerometer. Buy a better Device", Toast.LENGTH_SHORT).show();
+			return false;
+		} else
+		{
+			accelerometer = defaultAccelerometer;
+			Toast.makeText(this, "Accelerometer Set", Toast.LENGTH_SHORT).show();
+			return true;
+		}
+//		TextView stream = (TextView) context.findViewById(R.id.stream);
+//		stream.setText(displaySensorList.toString());
+	}
+
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1)
+	{
+
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event)
+	{
+		if(accelerometer!= null && event.sensor.getType() == accelerometer.getType())
+		{	
+			shakeDetect(event);
+		}
+		else if(proximitySensor!=null && event.sensor.getType() == proximitySensor.getType())
+		{
+			detectProximity(event);
+		}
+	}
+
+	private void detectProximity(SensorEvent event)
+	{
+		// TODO Auto-generated method stub
+		if(!started && event.values[0] == 0)
+		{
+			started=true;
+			startTimerNow();
+		}
+//		((TextView) findViewById(R.id.stream)).setText("x:"+event.values[0]+" y:"+event.values[1]+" z:"+event.values[2]);
+	}
+
+	private void shakeDetect(SensorEvent event)
+	{
+//			((TextView) findViewById(R.id.state)).setText("x:"+event.values[0]+" y:"+event.values[1]+" z:"+event.values[2]);
+	}
+	
+	protected final void startTimerNow()
+	{
+		new CountDownTimer((time * 1000), 100) {
+		
+		int seconds = time;
+		int mins, secs;
+		String s;
+		
+	     public void onTick(long millisUntilFinished) {
+	    	 if (Math.round((float)millisUntilFinished / 1000.0f) != seconds){
+	    		 mins = --seconds/60;
+	    		 secs = seconds%60;
+	    		 if(secs < 10)
+	    			 s = "0" + secs;
+	    		 else
+	    			 s = Integer.toString(secs);
+	    		 
+	             mintv.setText(Integer.toString(mins));
+	             sectv.setText(s);
+	    	 }
+	     }
+
+	     public void onFinish() {
+	       
+	     }
+	  }.start();
+	
+	  	mintv.setText(Integer.toString(time/60));
+	  	sectv.setText(Integer.toString(time%60));
+	}
+	
+
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -44,34 +182,11 @@ public class MainActivity extends Activity {
 	
 	View.OnClickListener startTimer = new OnClickListener(){
 		public void onClick(View v){
-			new CountDownTimer((time * 1000), 100) {
-				int seconds = time;
-				int mins, secs;
-				String s;
-				
-			     public void onTick(long millisUntilFinished) {
-			    	 if (Math.round((float)millisUntilFinished / 1000.0f) != seconds){
-			    		 mins = --seconds/60;
-			    		 secs = seconds%60;
-			    		 if(secs < 10)
-			    			 s = "0" + secs;
-			    		 else
-			    			 s = Integer.toString(secs);
-			    		 
-			             mintv.setText(Integer.toString(mins));
-			             sectv.setText(s);
-			    	 }
-			     }
-
-			     public void onFinish() {
-			       
-			     }
-			  }.start();
 			
-			  	mintv.setText(Integer.toString(time/60));
-			  	sectv.setText(Integer.toString(time%60));
+			startTimerNow();
 		}
 	};
+	
 	
 	View.OnClickListener resetTimer = new OnClickListener(){
 		public void onClick(View v){
@@ -80,4 +195,26 @@ public class MainActivity extends Activity {
 			sectv.setText(Integer.toString(time%60));
 		}
 	};
+	
+	@Override
+	protected void onResume()
+	{
+			super.onResume();
+			if(accelerometer!=null)
+			{
+				sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+			}
+			if(proximitySensor!=null)
+			{
+				sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+			}			
+	}
+	
+	@Override
+	protected void onPause()
+	{
+		super.onPause();
+		sensorManager.unregisterListener(this);
+	}
+
 }
